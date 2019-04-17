39950f34d0933564effe660b96d577ed53a2a421
hanxiao <sunshen.hx@gmail.com>
2019-04-16 21:41:39 -0400

    upload images

--- main.cpp
+++ main.cpp
@@ -33,9 +33,42 @@ int main(int argc, char *argv[]){
     set_rotation_index(uv,F,R,0);
     Eigen::MatrixXi Fn;
     locally_injective_map(V,F,P,R,T,uv,Fn);
+    
     // Plot the mesh
     igl::opengl::glfw::Viewer viewer;
-    viewer.data().set_mesh(uv, Fn);
+    viewer.data().set_mesh(uv,Fn);
+    viewer.append_mesh();
+    viewer.data().set_mesh(V,Fn);
+    unsigned int left_view, right_view;
+    int cube_id = viewer.data_list[0].id;
+    int sphere_id = viewer.data_list[1].id;
+    viewer.callback_init = [&](igl::opengl::glfw::Viewer &)
+    {
+      viewer.core().viewport = Eigen::Vector4f(0, 0, 640, 800);
+      left_view = viewer.core_list[0].id;
+      right_view = viewer.append_core(Eigen::Vector4f(640, 0, 640, 800));
+      return false;
+    };
+
+    viewer.callback_key_down = [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod)
+    {
+      if(key == GLFW_KEY_SPACE)
+      {
+        // By default, when a core is appended, all loaded meshes will be displayed in that core.
+        // Displaying can be controlled by calling viewer.data().set_visible().
+        viewer.data(cube_id).set_visible(false, left_view);
+        viewer.data(sphere_id).set_visible(false, right_view);
+        viewer.core_list[0].align_camera_center(uv);
+        viewer.core_list[1].align_camera_center(V);
+      }
+      return false;
+    };
+
+    viewer.callback_post_resize = [&](igl::opengl::glfw::Viewer &v, int w, int h) {
+      v.core( left_view).viewport = Eigen::Vector4f(0, 0, w / 2, h);
+      v.core(right_view).viewport = Eigen::Vector4f(w / 2, 0, w - (w / 2), h);
+      return true;
+    };
     //viewer.data().set_face_based(true);
     viewer.launch();
 }
